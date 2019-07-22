d05533a031c8babad3ade01a96aeeb249c9cc0b2
triggering-event <gabriel.j.shapiro@gmail.com>
2019-07-21 21:16:27 -0400

    adds Header component and updates task list in README

--- components/Header/index.js
+++ components/Header/index.js
@@ -3,10 +3,35 @@
 // Using a function create the component you see below:
 //
 //  <div class="header">
-//    <span class="date">SMARCH 28, 2019</span>
+//    <span class="date">MARCH 28, 2019</span>
 //    <h1>Lambda Times</h1>
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+    let headerDiv = document.createElement("div");
+    headerDiv.classList.add("header");
+
+    let headerDateSpan = document.createElement("span");
+    headerDateSpan.classList.add("date");
+    headerDateSpan.textContent = "March 28, 2019";
+    headerDiv.appendChild(headerDateSpan);
+
+    let headerTitle = document.createElement("h1");
+    headerTitle.textContent = "Lambda Times";
+    headerDiv.appendChild(headerTitle);
+
+    let headerTempSpan = document.createElement("span");
+    headerTempSpan.classList.add("temp");
+    headerTempSpan.textContent = "98°";
+    headerDiv.appendChild(headerTempSpan);
+
+    let headerContainer = document.querySelector(".header-container");
+    headerContainer.appendChild(headerDiv);
+
+    return headerDiv;
+}
+
+Header();
+
