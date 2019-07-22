08449841fc88c847842332f30a67dc2a6aeaa795
triggering-event <gabriel.j.shapiro@gmail.com>
2019-07-21 22:08:59 -0400

    adds card component to dynamically add articles and updates README

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,53 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(successData => {
+
+        let articlesObj = successData.data.articles;
+
+        for (let articleType in articlesObj) {
+           let articleTypeArray = articlesObj[articleType];
+           console.log(articleTypeArray);
+            articleTypeArray.forEach(articleObj => cardMaker(articleObj));
+        }
+
+    })
+
+    .catch(failData => {
+        console.log(failData);
+    })
+
+const cardMaker = (cardDataObj) => {
+    let cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+
+    let cardHeadlineDiv = document.createElement("div");
+    cardHeadlineDiv.classList.add("headline");
+    cardHeadlineDiv.textContent = cardDataObj.headline;
+    cardDiv.appendChild(cardHeadlineDiv);
+
+    let cardAuthorDiv = document.createElement("div");
+    cardAuthorDiv.classList.add("author");
+    cardDiv.appendChild(cardAuthorDiv);
+
+    let cardImgContainerDiv = document.createElement("div");
+    cardImgContainerDiv.classList.add("img-container");
+    cardAuthorDiv.appendChild(cardImgContainerDiv);
+
+    let cardImg = document.createElement("img");
+    cardImg.src = cardDataObj.authorPhoto;
+    cardImgContainerDiv.appendChild(cardImg);
+
+    let authorNameSpan = document.createElement("span");
+    authorNameSpan.textContent = cardDataObj.authorName;
+    cardAuthorDiv.appendChild(authorNameSpan);
+
+    const cardsContainerDiv = document.querySelector(".cards-container");
+
+    cardsContainerDiv.appendChild(cardDiv);
+
+    return cardDiv;
+
+}
