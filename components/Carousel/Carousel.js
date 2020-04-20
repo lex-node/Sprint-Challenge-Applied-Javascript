c467e353f463041950b1251f48f3b36d47be1ccb
triggering-event <gabriel.j.shapiro@gmail.com>
2020-04-19 21:24:13 -0400

    completes Carousel (stretch)

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,51 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+const imgArr = ["./assets/carousel/mountains.jpeg", "./assets/carousel/computer.jpeg", "./assets/carousel/trees.jpeg", "./assets/carousel/turntable.jpeg"]
+
+const CarouselCreator = (imgArr) => {
+    //create carousel container
+    const carouselDiv = document.createElement('div');
+    carouselDiv.classList.add('carousel');
+
+    //create buttons
+    const leftButton = document.createElement('div');
+    leftButton.textContent = '<'
+    leftButton.classList.add('left-button');
+    const rightButton = document.createElement('div');
+    rightButton.textContent = '>'
+    rightButton.classList.add('right-button');
+    carouselDiv.appendChild(rightButton);
+    carouselDiv.appendChild(leftButton);
+
+
+    imgArr.map((img, index) => {
+        const imgDiv = document.createElement('img');
+        imgDiv.classList.add(`img-${index}`);
+        index === 0 && (imgDiv.style.display = 'flex');
+        imgDiv.src = img;
+        return carouselDiv.appendChild(imgDiv);
+    })
+
+    //add clickhandlers to buttons
+    let currentImgIndex = 0;
+    leftButton.addEventListener('click', event => {
+        event.preventDefault();
+        document.querySelector(`.img-${currentImgIndex}`).style.display = 'none';
+        currentImgIndex === 0 ? currentImgIndex = 3 : currentImgIndex--;
+        document.querySelector(`.img-${currentImgIndex}`).style.display = 'flex';
+    });
+
+    rightButton.addEventListener('click', event => {
+        event.preventDefault();
+        document.querySelector(`.img-${currentImgIndex}`).style.display = 'none';
+        currentImgIndex === 3 ? currentImgIndex = 0 : currentImgIndex++;
+        document.querySelector(`.img-${currentImgIndex}`).style.display = 'flex';
+    });
+
+
+    return carouselDiv;
+}
+
+document.querySelector('.carousel-container').appendChild(CarouselCreator(imgArr));
